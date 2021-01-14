750626279f3c88098687e80a1a73004b07bc974f
Herbe Malveillante <herbemalveillante@gmail.com>
2021-01-14 20:10:56 +0100

    adding links

--- cogs/infocommands.py
+++ cogs/infocommands.py
@@ -16,7 +16,7 @@ class InfoCog(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 	
-	@commands.command(name="info", aliases = ["i", "uptime", "stats"], description = "Retrieves informations about the bot and the server running it")
+	@commands.command(name="info", aliases = ["i", "uptime", "stats", "invite", "link", "links"], description = "Retrieves informations about the bot and the server running it")
 	async def info(self, ctx):
 		uptime = int(datetime.datetime.now().timestamp() - config.boottime)
 		min, sec = divmod(uptime, 60)
@@ -39,7 +39,11 @@ class InfoCog(commands.Cog):
 		Users count : `{len(self.bot.users)}`
 		Players count : `{database.get_player_number()}`
 		""", inline=False)
-		embed.add_field(name="Links :", value = f"[Invite me on your server](https://discord.com/oauth2/authorize?client_id={self.bot.user.id}&scope=bot&permissions=379969)", inline = False)
+		embed.add_field(name="Links :", value = f"""
+		[Invite me on your server](https://discord.com/oauth2/authorize?client_id={self.bot.user.id}&scope=bot&permissions=379969)
+		[Official support server](https://discord.gg/vRA4gdraaC)
+		[GitHub repo](https://github.com/HerbeMalveillante/MMOBOT)
+		""", inline = False)
 		await ctx.send(embed=embed)
 		log(f"{ctx.author} asked for infos about the bot.")
 	
